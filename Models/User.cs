d95968462d5fbf9a0793de530220f1ca9a557cff
LorandoZ <15999868+LorandoZ@users.noreply.github.com>
2020-05-14 16:19:22 +0800

    init & add Users

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformationServiceBackEnd.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string NickName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
