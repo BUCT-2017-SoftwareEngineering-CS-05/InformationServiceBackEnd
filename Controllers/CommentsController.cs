2f9d4fb5e37880ba914647379ecb335e159ef7b7
LorandoZ <15999868+LorandoZ@users.noreply.github.com>
2020-05-18 17:06:22 +0800

    add comment edit method

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -51,31 +51,17 @@ namespace InformationServiceBackEnd.Controllers
         // PUT: api/Comments/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutComment(int id, Comment comment)
+        [HttpPut("{midex}/{userid}")]
+        public async Task<IActionResult> PutComment(int midex,string userid, Comment comment)
         {
-            if (id != comment.midex)
+            if (midex != comment.midex)
             {
                 return BadRequest();
             }
 
             _context.Entry(comment).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CommentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
